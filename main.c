d4c0cb2d2c5ea9c559f452aa808cfd1c75f0d4f9
as <johann_albert.scherzer@gmx.de>
2023-12-02 18:13:10 +0100

    added the algorithms and the housekeeping in order to implement good rotation

--- main.c
+++ main.c
@@ -15,42 +15,52 @@
 #define CYAN COLOR_CYAN
 #define WHITE COLOR_WHITE
 
-struct status {
-	int curx, cury;
-	short shape;
-};
-
 struct block {
 	int x, y;
 };
 
+// The blocks of the shape are reside in a coordinate system like this
+//   0 1 2
+// 0
+// 1
+// The x and the y value of a block specifies it's coordinate in that system.
+// These coordinate systems are later transformed into the 10x20 coordinate system of the board
 typedef struct {
 	struct block blocks[4];
 	int color;
+	bool flipped;
+	short index;
 } shape_t;
 
+struct status {
+	int curx, cury;
+	shape_t shape;
+};
+
 static shape_t shapes[7] = {
-	{{{0, 0}, {1, 0}, {2, 0}, {0, 1}}, MAGENTA},
-	{{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, RED},
-	{{{0, 0}, {1, 0}, {2, 0}, {2, 1}}, WHITE},
-	{{{1, 0}, {2, 0}, {0, 1}, {1, 1}}, GREEN},
-	{{{0, 0}, {1, 0}, {1, 1}, {2, 1}}, CYAN},
-	{{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, BLUE},
-	{{{0, 0}, {1, 0}, {2, 0}, {1, 1}}, YELLOW},
+	{{{0, 0}, {1, 0}, {2, 0}, {3, 0}}, RED, false, 1},
+	{{{0, 0}, {1, 0}, {2, 0}, {2, 1}}, WHITE, false, 2},
+	{{{1, 0}, {2, 0}, {0, 1}, {1, 1}}, GREEN, false, 3},
+	{{{0, 0}, {1, 0}, {1, 1}, {2, 1}}, CYAN, false, 4},
+	{{{0, 0}, {1, 0}, {0, 1}, {1, 1}}, BLUE, false, 5},
+	{{{0, 0}, {1, 0}, {2, 0}, {1, 1}}, YELLOW, false, 6},
 };
 
 static int board[NUMROWS][NUMCOLS];
 static short level = 1;
 
 void drawboard();
-bool allowed(int, int, short);
-void eraseshape(int, int, short);
+bool allowed(int, int, shape_t *);
+void eraseshape(int, int, shape_t *);
 void move_down(uv_timer_t *);
+static void real_rotate(shape_t *, bool);
+static void rotate(shape_t *);
+void input(uv_idle_t *);
 
 uv_loop_t * loop;
 
 int main() {
-	struct status status = {3, 0, 0};
+	struct status status = {3, 0, shapes[0]};
 
 	uv_timer_t timer;
 
@@ -65,23 +75,27 @@ int main() {
 	init_pair(WHITE, COLOR_BLACK, WHITE);
 	init_pair(BLACK, COLOR_WHITE, COLOR_BLACK);
 	curs_set(0);
+	noecho();
 	move(0, 0);
+	
 	for (int i = 0; i < NUMROWS; i++) {
 		for (int j = 0; j < NUMCOLS; j++) {
 			board[i][j] = BLACK;
 		}
 	}
 	for (int i = 0; i < 4; i++) {
-		board[status.cury + shapes[status.shape].blocks[i].y][status.curx + shapes[status.shape].blocks[i].x] = shapes[status.shape].color; 
+		board[status.cury + status.shape.blocks[i].y][status.curx + status.shape.blocks[i].x] = status.shape.color; 
 	}
 	drawboard();
 	refresh();
 	
 	loop = uv_default_loop();
 	uv_loop_init(loop);
+	loop->data = (void *) &status;
+	
 	uv_timer_init(loop, &timer);
-	timer.data = (void *) &status;
 	uv_timer_start(&timer, move_down, TIME, TIME);
+	
 	uv_run(loop, UV_RUN_DEFAULT);
 	
 	uv_loop_close(loop);
@@ -101,34 +115,77 @@ void drawboard() {
 	move(0, 0);
 }
 
-bool allowed(int x, int y, short shape) {
+bool allowed(int x, int y, shape_t * shape) {
 	for (int i = 0; i < 4; i++) {
-		if (x + shapes[shape].blocks[i].x >= NUMCOLS
-		 || y + shapes[shape].blocks[i].y >= NUMROWS) {
+		if (x + shape->blocks[i].x >= NUMCOLS
+		 || y + shape->blocks[i].y >= NUMROWS
+		 || x + shape->blocks[i].x < 0
+		 || y + shape->blocks[i].y < 0) {
 		 return false;
 		}
 	}
 	return true;
 }
 
-void eraseshape(int x, int y, short shape) {
+void eraseshape(int x, int y, shape_t * shape) {
 	for (int i = 0; i < 4; i++) {
-		board[y + shapes[shape].blocks[i].y][x + shapes[shape].blocks[i].x] = BLACK;
+		board[y + shape->blocks[i].y][x + shape->blocks[i].x] = BLACK;
 	}
 }
 
 void move_down(uv_timer_t * handle) {
-	struct status * data = (struct status *) handle->data;
-	if (allowed(data->curx, data->cury+1, data->shape)) {
-		eraseshape(data->curx, data->cury, data->shape);
+	struct status * data = (struct status *) loop->data;
+	if (allowed(data->curx, data->cury+1, &(data->shape))) {
+		eraseshape(data->curx, data->cury, &(data->shape));
 		data->cury++;
 		for (int i = 0; i < 4; i++) {
-			board[data->cury + shapes[data->shape].blocks[i].y][data->curx + shapes[data->shape].blocks[i].x] = shapes[data->shape].color;
+			board[data->cury + data->shape.blocks[i].y][data->curx + data->shape.blocks[i].x] = data->shape.color;
 		}
 		drawboard();
 		refresh();
 	} else {
 		uv_timer_stop(handle);
 		uv_stop(loop);
+	}
+}
+
+// This rotation code is basically copied from Abraham vd Merwe <abz@blio.net>
+// fantastic tetris implementation tint (github.com/DavidGriffith/tint) (check
+// it out). I mainly adopted it to my (in my opionion more intuitive coordinate system starting a 0 and not -1.
+// Anyway thankyou from me for that source of inspiration.
+static void real_rotate(shape_t * shape, bool clockwise) {
+	if (clockwise) {
+		for (int i = 0; i < 4; i++) {
+			int tmp = shape->blocks[i].x;
+			shape->blocks[i].x = -(shape->blocks[i].y - 1);
+			shape->blocks[i].y = tmp-1;
+		}
+	} else {
+		for (int i = 0; i < 4; i++) {
+			int tmp = shape->blocks[i].x;
+			shape->blocks[i].x = shape->blocks[i].y - 1;
+			shape->blocks[i].y = -(tmp-1);
+		}
+	}
+}
+
+static void rotate(shape_t * shape) {
+	switch (shape->index) {
+		case 6:
+		case 0:
+		case 2:
+			real_rotate(shape, false);
+			break;
+		case 5:
+			break;
+		case 4:
+			if (shape->flipped) real_rotate(shape, true); else real_rotate(shape, false);
+			shape->flipped = !shape->flipped;
+			break;
+		case 3:
+		case 1:
+			if (shape->flipped) real_rotate(shape, false); else real_rotate(shape, true);
+			shape->flipped = !shape->flipped;
+			break;
 	}
 }
